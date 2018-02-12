ab244f59731b6d8f005b1c11d8ffd6ab4ba3e8b5
ajboxjr <ajboxjr@gmail.com>
2018-02-12 10:42:34 -0500

    Remove Google Sign in

--- app/scenes/LoginScene.js
+++ app/scenes/LoginScene.js
@@ -1,8 +1,7 @@
 import React,{ Component } from 'react'
-import { Text, TextInput, Image, View, StyleSheet, TouchableOpacity, TouchableHighlight } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, TouchableHighlight } from 'react-native'
 import { Actions as RouteActions} from 'react-native-router-flux';
 import LoginContainer from 'containers/LoginContainer'
-import GoogleSignIn from 'components/GoogleSignIn'
 
 class LoginScene extends React.Component {
   constructor(props){
@@ -12,12 +11,7 @@ class LoginScene extends React.Component {
   render(){
     return(
       <View style={styles.container}>
-        <View style={styles.ImageContainer}>
-          <Image style={styles.Logo} source={require('assets/Dream_Catcher.png')} />
-        </View>
         <LoginContainer />
-        <GoogleSignIn />
-
       </View>
     )
   }
@@ -25,23 +19,10 @@ class LoginScene extends React.Component {
 
 const styles = StyleSheet.create({
   container:{
-    backgroundColor: '#3B4EE3',
     flex:1,
+    backgroundColor: '#3B4EE3',
     alignItems: 'center',
     justifyContent:'space-between'
-  },
-  ImageContainer:{
-    flex:.5,
-    justifyContent:'center'
-  },
-  Logo : {
-    justifyContent: 'center',
-  },
-  Slogan:{
-    flex:1,
-    textAlign: 'center',
-    paddingTop: '5%',
-    fontSize: 24
   },
 })
 export default LoginScene;
