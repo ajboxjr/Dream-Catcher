fa6da952a7437a9db99123af981729520db0db70
ajboxjr <ajboxjr@gmail.com>
2018-04-16 18:14:03 -0400

    remove flux and add react navigation with custom actions

--- App.js
+++ App.js
@@ -1,59 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Actions as RouteActions, Scene, Stack, Router, TabBar, Icon, Tabs} from 'react-native-router-flux';
+// import { Actions as RouteActions, Scene, Stack, Router, TabBar, Icon, Tabs} from 'react-native-router-flux';
+import { StackNavigator, TabNavigator, addNavigationHelpers, NavigationActions } from 'react-navigation';
 
 import { Provider } from 'react-redux';
+import SettingScene from 'scenes/SettingScene';
 
-import configureStore from 'store/store'
+import configureStore from './app/store/store'
 const store = configureStore({});
 
-import NewDreamIcon from 'icons/NewDreamIcon'
-import ProfileIcon from 'icons/ProfileIcon'
-import ListIcon from 'icons/ListIcon'
-
-//Login Scene
-import LoginScene from 'scenes/LoginScene'
-// Home Scenes
-import DreamListScene from 'scenes/DreamListScene'
 import NewDreamScene from 'scenes/NewDreamScene'
-import ProfileScene from 'scenes/ProfileScene'
-import DreamEntryScene from 'scenes/DreamEntryScene'
-import SettingScene from 'scenes/SettingScene'
+import NewDreamIcon from './app/components/icons/NewDreamIcon'
+import ProfileIcon from './app/components/icons/ProfileIcon'
+import ListIcon from './app/components/icons/ListIcon'
 
-store.subscribe(() => {
-  state = store.getState()
-  if(state.user.token !== null){
-    // TODO: Authenticate token with database
-    RouteActions.main_menu()
-  }
-  else{
-    RouteActions.login_menu()
-  }
-})
+import RootNavigationWithState from './app/navigation/RootNavigator'
 
+  // <RootNavigationWithState />
 export default class App extends React.Component {
   render() {
+    console.ignoredYellowBox = ['Remote debugger'];
     return (
       <Provider store={store}>
-        <Router>
-          <Stack key="root">
-            <Scene key="login_menu" tabBarStyle={{ backgroundColor: '#eee', alignItems: 'center' }} hideNavBar={true} navTransparent={1}>
-              <Scene key="scene_1" component={LoginScene} title="login" />
-            </Scene>
-            <Tabs key="main_menu" tabBarStyle={{ backgroundColor: '#eee', alignItems: 'flex-end' }} >
-              <Scene key="tab1" title=" " icon={ListIcon}>
-                <Scene key="tab1_scene2" component={DreamListScene} navTransparent={1} hideNavBar={true} />
-                <Scene key="tab2_scene2" component={DreamEntryScene} title="Dream Entry" navTransparent={1} hideNavBar={true} />
-              </Scene>
-              <Scene key="tab2" title=" " icon={NewDreamIcon}>
-                <Scene key="tab2_scene1" component={NewDreamScene} hideNavBar={true} initial />
-              </Scene>
-              <Scene key="tab3" title=" " icon={ProfileIcon}>
-                <Scene key="tab3_scene1" component={ProfileScene} hideNavBar={true} intial />
-              </Scene>
-            </Tabs>
-          </Stack>
-        </Router>
+        <RootNavigationWithState />
       </Provider>
     );
   }
