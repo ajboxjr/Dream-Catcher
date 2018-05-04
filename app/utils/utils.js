c8ba5762b42d14e3f69501692533c7c3585b5f4b
ajboxjr <ajboxjr@gmail.com>
2018-05-03 20:14:58 -0400

    add catchphrase unexport functions

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,34 +1,25 @@
+import React from 'react';
 import {
   createReactNavigationReduxMiddleware,
-  createReduxBoundAddListener,
+  createReduxBoundAddListener
 } from 'react-navigation-redux-helpers';
-import React from 'react';
-export const mapNavigationStateParamsToProps = (SomeComponent) => {
-  return class extends React.Component {
-    static navigationOptions = SomeComponent.navigationOptions; // better use hoist-non-react-statics
-    render () {
-      const {navigation: {state: {params}}} = this.props;
-      return <SomeComponent {...params} {...this.props} />;
-    }
-  };
-};
 
-export const middleware = createReactNavigationReduxMiddleware(
+const middleware = createReactNavigationReduxMiddleware(
   "root",
   state => state.rootNav
 );
 
-export const addListener = createReduxBoundAddListener("root");
+const addListener = createReduxBoundAddListener("root");
 
-export const Month = ["January", "February", "March", "April", "May", "June",
+const Month = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
-// export const Colors = ['#3ED67F', '#56CCF2', '#EBB617', '#9B51E0', '#EB5757', '#2D9CDB','#FF7800', '#3DFF45', '#F2994A','#AF07B2']
+
 const getRandomInt = (top) => {
   return Math.floor(Math.random() * Math.floor(top));
 }
 
-export const Colors = () => {
+const Colors = () => {
     let ItemColors = ["#CF7474", "#2D9CDB", "#3ED67F", "#1FD2DE", "#12FF2A", "#EB5757", "#08FFE1", "#FF7E08"]
     let randomColorArr = []
     while (ItemColors.length){
@@ -40,7 +31,26 @@ export const Colors = () => {
     // console.log(randomColorArr);
     return randomColorArr
   }
-  export const getCatchPhrase = () => {
-    let catchPhrase = ["Say Something Dreamy","Tell Me Everything","So What Happened","Morning","One Sheep Two Sheep..."]
-    return catchPhrase[getRandomInt(catchPhrase.length)]
-  }
+
+const getCatchPhrase = () => {
+  let catchPhrase = ["Say Something Dreamy","Tell Me Everything","So What Happened","Morning","One Sheep Two Sheep..."]
+  return catchPhrase[getRandomInt(catchPhrase.length)]
+}
+
+// const mapNavigationStateParamsToProps = (SomeComponent) => {
+//   return class extends React.Component {
+//     static navigationOptions = SomeComponent.navigationOptions; // better use hoist-non-react-statics
+//     render () {
+//       const {navigation: {state: {params}}} = this.props;
+//       return <SomeComponent {...params} {...this.props} />;
+//     }
+//   };
+// };
+
+module.exports = {
+  middleware: middleware,
+  addListener: addListener,
+  Month: Month,
+  Colors:Colors,
+  getCatchPhrase: getCatchPhrase
+}
