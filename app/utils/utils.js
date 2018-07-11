b4f67771608a0509a739c7de1f2cda55ce126841
ajboxjr <ajboxjr@gmail.com>
2018-07-11 13:55:37 -0400

    remove date format & navstate to props

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,51 +1,37 @@
-
-
-const Month = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-
-const formatDate = (updatedDate) =>{
-  date = new Date(updatedDate)
-  formatedDate = Month[date.getMonth()-1]+" "+date.toLocaleString('en-US', { day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })
-  return formatedDate
-}
-
-//-----------
 const getRandomInt = (top) => {
   return Math.floor(Math.random() * Math.floor(top));
 }
 
-const Colors = () => {
-    let ItemColors = ["#CF7474", "#2D9CDB", "#3ED67F", "#1FD2DE", "#12FF2A", "#EB5757", "#08FFE1", "#FF7E08"]
-    let randomColorArr = []
-    while (ItemColors.length){
-      // console.log(ItemColors.length);
-      let index = getRandomInt(ItemColors.length)
-      randomColorArr.push(ItemColors[index])
-      ItemColors.splice(index,1)
-    }
-    // console.log(randomColorArr);
-    return randomColorArr
+/*
+  Randomly sort colors in new array
+*/
+const Colors = (count) => {
+  let ItemColors = [
+    "#CF7474",
+    "#2D9CDB",
+    "#3ED67F",
+    "#1FD2DE",
+    "#12FF2A",
+    "#EB5757",
+    "#08FFE1",
+    "#FF7E08"
+  ]
+  let randomColorArr = []
+  for (var i = 0; i < count; i++) {
+    let index = getRandomInt(ItemColors.length)
+    randomColorArr.push(ItemColors[index])
+    // ItemColors.splice(index,1)
   }
+  console.log(randomColorArr);
+  return randomColorArr
+}
 
 const getCatchPhrase = () => {
-  let catchPhrase = ["Say Something Dreamy","Tell Me Everything","So What Happened","Morning","One Sheep Two Sheep..."]
+  let catchPhrase = ["Say Something Dreamy", "Tell Me Everything", "So What Happened", "Morning", "One Sheep Two Sheep..."]
   return catchPhrase[getRandomInt(catchPhrase.length)]
 }
 
-// const mapNavigationStateParamsToProps = (SomeComponent) => {
-//   return class extends React.Component {
-//     static navigationOptions = SomeComponent.navigationOptions; // better use hoist-non-react-statics
-//     render () {
-//       const {navigation: {state: {params}}} = this.props;
-//       return <SomeComponent {...params} {...this.props} />;
-//     }
-//   };
-// };
-
 module.exports = {
-  Month: Month,
-  Colors:Colors,
-  getCatchPhrase: getCatchPhrase,
-  formatDate: formatDate
+  Colors: Colors,
+  getCatchPhrase: getCatchPhrase
 }
